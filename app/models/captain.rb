6b035d989b9c863dfb4a539b7af71074cfd75909
Sam Markham <sam.e.markham@gmail.com>
2019-11-17 20:59:08 -0800

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,23 +2,26 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 
   def self.sailors
-    # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
   end
 
   def self.motorboat_operators
-    # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
   end
 
   def self.talented_seafarers
+    where(id: [self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id)])
+
+    #this one was from the lab and is a slightly more SQL way of doing it--one above is from Lantz and a bit more AREL-y
     # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
-    # where.not("id IN (?)", self.sailors.pluck(:id))
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 
 end
